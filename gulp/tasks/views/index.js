e1d1fe2347c9ecf4c671cf44048d1a6aa888224b
monochromer <monochromer@mail.ru>
2017-09-01 00:49:47 +0500

    смена места переименования файлов в таске views

--- gulp/tasks/views/index.js
+++ gulp/tasks/views/index.js
@@ -13,13 +13,18 @@ module.exports = (gulp, plugins, config) => (done) => {
 
   return plugins.combiner(
     gulp.src(config.src, { since: gulp.lastRun(config.taskName) }),
+    // plugins.rename(path => {
+    //   let { dirname, basename } = path;
+    //   path.basename = basename === 'index' ? dirname : `${dirname}-${basename}`;
+    //   path.dirname = '.';
+    // }),
+    plugins.pug(config.engineOptions),
+    plugins.prettify(config.prettify),
     plugins.rename(path => {
       let { dirname, basename } = path;
       path.basename = basename === 'index' ? dirname : `${dirname}-${basename}`;
       path.dirname = '.';
     }),
-    plugins.pug(config.engineOptions),
-    plugins.prettify(config.prettify),
     gulp.dest(config.dest),
     plugins.if(
       !!plugins.browserSync.active,
